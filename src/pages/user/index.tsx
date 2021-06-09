9182308dbb532a76fdb423f7414271e0e405c959
duanhejin <duanhejin@zhoupudata.com>
2021-06-09 18:24:38 +0800

    init

---
+++ src/pages/user/index.tsx
@@ -0,0 +1,23 @@
+ 
+/*
+ * @Description: xx模块
+ * @Company: zhoupudata
+ * @Author: duanhejin
+ * @Date: 2021-06-09 18:02:13
+ * @LastEditors: duanhejin
+ * @LastEditTime: 2021-06-09 18:02:29
+ */
+
+import * as React from 'react';
+
+export interface IUserProps {
+}
+
+export default function User (props: IUserProps) {
+  return (
+    <div>
+      User
+    </div>
+  );
+}
+
